c402ee68710906e1561910fac8371f2f59ad1915
artates <nicolasartates@gmail.com>
2023-08-17 21:01:42 -0700

    Messed with player controller. I know what I need to do next and it is in gamelog. reference other builds of the gameplay scene to make it work correctly.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,10 +13,6 @@ public class PlayerController : MonoBehaviour
 
     public GameController GameController;
 
-    //code for switching the color of the player sprite
-    private Renderer rend;
-    private Color newColor = Color.red;
-
     //GameObject for the Player game object. This is so I can manipulate the position in Fixed Update Below
     public GameObject Player;
 
@@ -24,37 +20,9 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         //Initialize Player Game object
         Player = GameObject.Find("Player");
         body = GetComponent<Rigidbody2D>(); //MIGHT NOT NEED THIS ANYMORE
-
-        //Code for switching the color
-        //switches the new color var based on player settings default red 
-        //MIGHT NOT NEED THIS ANYMORE
-        if (PlayerSettings.Instance.color == 0)
-        {
-            newColor = Color.red;
-           
-        }
-        else if (PlayerSettings.Instance.color == 1)
-        {
-            newColor = Color.cyan;
-            
-            
-        }
-        else if (PlayerSettings.Instance.color == 2)
-        {
-            
-            newColor = Color.magenta;
-            
-        }
-
-        //instantiates and uses renderer to Change sprite color
-        rend = GetComponent<Renderer>();
-        rend.material.color = newColor; //MIGHT NOT NEED THIS ANYMORE
-
-
     }
 
     // Update is called once per frame. Handles the controls for the player. 
@@ -62,19 +30,19 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))//on w
         {
-            Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + .05f, Player.transform.position.z);
+            Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + .1f, Player.transform.position.z);
         }
         if (Input.GetKey(KeyCode.S)) //on s
         {
-            Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y - .05f, Player.transform.position.z);
+            Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y - .1f, Player.transform.position.z);
         }
         if (Input.GetKey(KeyCode.LeftShift)|| Input.GetMouseButton(0)) //on shift Changes color
         {
-            rend.material.color = Color.red;
+           
         }
         if (Input.GetKey(KeyCode.Space) || Input.GetMouseButton(1))//on space changes color 
         {
-            rend.material.color = Color.blue;
+            
         }
 
 
